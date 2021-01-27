09a99eb7b6be0cc69a78b9f7ed5c0f09b0e88a3f
atimilson <atimilson95@gmail.com>
2021-01-27 18:10:19 -0400

    adicionando arquivo de estilo

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,15 @@ import styled from 'styled-components'
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
 `
+
+// function Title(props) {
+//    return (
+//    <h1>{props.children}
+//    </h1>
+//    )
+// }
 
 export default function Home() {
   return <Title>My page</Title>
