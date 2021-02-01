06e93b29e90501406cbd5b6bb7d811a5e2a46b4c
atimilson <atimilson95@gmail.com>
2021-01-31 23:52:20 -0400

    aula 03 inicio

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 import db from '../db.json';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -25,15 +28,38 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();               
+  const [name, setName] = React.useState('');
   return (
     <QuizBackground backgroundImage={db.bg}>
+       <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
-          <Widget.Content>                                           
-            <p>{db.description}</p>
+          <Widget.Content>  
+            <form onSubmit={function(infosDoEvento) {
+              infosDoEvento.preventDefault(); 
+              router.push(`/quiz?name=${name}`)
+              console.log('fazendo submit');
+            }}>
+              <input 
+              onChange = {function (infosDoEvento){
+                console.log(infosDoEvento.target.value);
+
+               // name = infosDoEvento.target.value;
+               setName(infosDoEvento.target.value);
+              }}
+              type="text" placeholder="Diz ai seu nome"></input>
+              <button type="submit" disabled={name.length === 0}> 
+                Jogar 
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
---
+++ pages/quiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const titulo = styled.div`
+    color: #000000;
+`;
+
+export default function Quiz() {
+  return (
+    <titulo>
+      Página de quiz
+    </titulo>
+  );
+}
